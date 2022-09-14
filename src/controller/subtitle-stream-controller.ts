ba19f2f7e3f7fed70ac12e300a58388cacec99be
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2022-09-14 15:47:20 -0700

    Revert changes from pluto.5 and pluto.6

--- src/controller/subtitle-stream-controller.ts
+++ src/controller/subtitle-stream-controller.ts
@@ -173,45 +173,6 @@ export class SubtitleStreamController
       );
     }
   }
-  
-  // This is needed to correct the Xbox issue. The one where CC doesn't 
-  // display in VOD up to the point where they were enabled mid-stream. 
-  // https://github.com/cbsinteractive/avia-player-support/issues/246
-  onMediaSeeking() {
-    // Only applies to Xbox platform
-    if (!/Xbox; Xbox One/.test(navigator.userAgent)) {
-      return;
-    }
-
-    // Find the currently showing subtitle track
-    const tracks = Array.from<TextTrack>(this.media.textTracks);
-    const track = tracks.find(track => track.mode != 'disabled' && (track.kind == 'subtitles' || track.kind == 'captions'));
-
-    // Manually reset the cues and fragments
-    if (track && track.cues) {
-      // Clear all text track cues
-      Array.from(track.cues).forEach(cue => track.removeCue(cue));
-
-      // Clear all loaded subtitle fragments
-      this.fragmentTracker.removeFragmentsInRange(0, this.media.duration, PlaylistLevelType.SUBTITLE);
-
-      // Clear internal buffered lists
-      this.tracksBuffered.forEach((_, index, tracks) => tracks[index] = []);
-
-      // This is to address persistent cue issue for Pluto. Without it,
-      // the very last timed text cue before the seek is called remains 
-      // on the screen even after seek is completed. See ticket below: 
-      // https://github.com/cbsinteractive/avia-player-support/issues/258
-      if (track.mode === 'showing') { 
-        track.mode = 'hidden';
-        self.setTimeout(() => {
-            track.mode = 'showing';
-        }, 10);
-      }
-    }
-
-    this.fragPrevious = null;
-  }
 
   // If something goes wrong, proceed to next frag, if we were processing one.
   onError(event: Events.ERROR, data: ErrorData) {
